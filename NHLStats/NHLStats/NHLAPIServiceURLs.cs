62d1435c3fcfc946af61626717a076e31799ca62
Paul Laberge <plaberge@outlook.com>
2018-11-20 12:45:06 -0500

    Added NHLAPIServiceURLs class

---
+++ NHLStats/NHLStats/NHLAPIServiceURLs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHLStatsAPI
+{
+    public static class NHLAPIServiceURLs
+    {
+        public static string leagueStandings = "https://statsapi.web.nhl.com/api/v1/standings/byLeague";
+    }
+}
+
