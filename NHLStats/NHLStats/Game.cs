463af8a6c2a5eca91c7c1fdce1d7146b2239c8ea
Paul Laberge <plaberge@outlook.com>
2018-11-29 16:36:58 -0500

    Added constructor for Game class, added URL to NHLAPIServiceURLs class.

--- NHLStats/NHLStats/Game.cs
+++ NHLStats/NHLStats/Game.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NHLStats
@@ -31,9 +33,56 @@ namespace NHLStats
         }
 
         // Constructor for Game class from a specific Game ID
-        public Game(string gameID)
+        public Game(string theGameID)
         {
+            // Populate the gameID property
+            gameID = theGameID;
 
-        }
+            // Get the URL for the API call to a specific Game
+            string theGame = NHLAPIServiceURLs.specificGame;
+
+            // Replace placeholder value ("###") in the placeholder URL with the requested GameID.
+            gameLink = theGame.Replace("###", theGameID);
+
+            // Execute the API call
+            var json = DataAccessLayer.ExecuteAPICall(gameLink);
+
+            // Populate the rest of the Game class properties
+            gameType = 
+            season 
+            gameDate 
+            abstractGameState 
+            codedGameState 
+            detailedState 
+            statusCode 
+            homeTeam 
+            awayTeam 
+            gameVenue 
+            gameContent
+
+            //IList<JToken> results = json["gameData"][0]["game"][0].Children().ToList();
+
+
+        //newTeam.NHLTeamID = Convert.ToInt32(result["team"]["id"]);
+        //newTeam.TeamName = result["team"]["name"].ToString();
+        //newTeam.Wins = Convert.ToInt32(result["leagueRecord"]["wins"]);
+        //newTeam.Losses = Convert.ToInt32(result["leagueRecord"]["losses"]);
+        //newTeam.OvertimeLosses = Convert.ToInt32(result["leagueRecord"]["ot"]);
+        //newTeam.GoalsAgainst = Convert.ToInt32(result["goalsAgainst"]);
+        //newTeam.GoalsScored = Convert.ToInt32(result["goalsScored"]);
+        //newTeam.Points = Convert.ToInt32(result["points"]);
+        //newTeam.DivisionRank = Convert.ToInt32(result["divisionRank"]);
+        //newTeam.ConferenceRank = Convert.ToInt32(result["conferenceRank"]);
+        //newTeam.LeagueRank = Convert.ToInt32(result["leagueRank"]);
+        //newTeam.WildcardRank = Convert.ToInt32(result["wildcardRank"]);
+        //newTeam.ROW = Convert.ToInt32(result["row"]);
+
+
+
+
+
+
+
+    }
     }
 }
--- NHLStats/NHLStats/NHLAPIServiceURLs.cs
+++ NHLStats/NHLStats/NHLAPIServiceURLs.cs
@@ -8,6 +8,7 @@ namespace NHLStats
     {
         public static string leagueStandings = "https://statsapi.web.nhl.com/api/v1/standings/byLeague";
         public static string todaysGames = "https://statsapi.web.nhl.com/api/v1/schedule";
+        public static string specificGame = "https://statsapi.web.nhl.com/api/v1/game/###/feed/live";
     }
 }
 
