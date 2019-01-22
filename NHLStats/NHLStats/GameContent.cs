f0df03656e782da17d2671f3eae5bc165837a701
Paul Laberge <plaberge@outlook.com>
2019-01-22 14:11:33 -0500

    Added some URL data into GameContent.

--- NHLStats/NHLStats/GameContent.cs
+++ NHLStats/NHLStats/GameContent.cs
@@ -6,7 +6,11 @@ namespace NHLStats
 {
     public class GameContent
     {
+
+        public string previewTitle;
+
         //Example Content:  https://statsapi.web.nhl.com/api/v1/game/2018020323/content
+        //Image content:  https://nhl.bamcontent.com/images/photos/302168986/248x140/cut.jpg
 
         // Default Constructor
         public GameContent()
