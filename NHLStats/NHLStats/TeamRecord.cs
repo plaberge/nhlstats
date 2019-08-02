6ce46739b6bab4086f3a5c6c6f04d9d2b45de66c
Paul Laberge <plaberge@microsoft.com>
2019-08-02 14:10:43 -0400

    patch:  fixed issue in TeamRecord

--- NHLStats/NHLStats/TeamRecord.cs
+++ NHLStats/NHLStats/TeamRecord.cs
@@ -7,15 +7,12 @@ namespace NHLStats
     class TeamRecord : Team
     {
         public string season { get; set; }
-        public int DivisionRank { get; set; }
         public int DivisionL10Rank { get; set; }
         public int DivisionHomeRank { get; set; }
         public int DivisionRoadRank { get; set; }
-        public int ConferenceRank { get; set; }
         public int ConferenceL10Rank { get; set; }
         public int ConferenceHomeRank { get; set; }
         public int ConferenceRoadRank { get; set; }
-        public int LeagueRank { get; set; }
         public int LeagueL10Rank { get; set; }
         public int LeagueHomeRank { get; set; }
         public int LeagueRoadRank { get; set; }
